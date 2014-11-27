ade69ac24faf7b5cf29cf54df8eb93c7b3984043
Memphiz <memphis@machzwo.de>
2014-11-27 13:53:38 +0100

    [droid] - add android platform for libboblight.so download (arm only atm)

--- script.xbmc.boblight/default.py
+++ script.xbmc.boblight/default.py
@@ -134,7 +134,7 @@ class Main():
                             localize(32506))
       
       else:
-        # ask user if we should fetch the lib for osx, ios and windows
+        # ask user if we should fetch the lib for osx, ios, android and windows
         if xbmcgui.Dialog().yesno(__scriptname__,
                                   localize(32504),
                                   localize(32509)):
